29c80e6ee27e650b75af4f602b6e28262d42bf2b
“sbudani-cloud” <sbudani@alumno.huergo.edu.ar>
2026-03-05 15:05:25 -0300

    holi?

---
+++ Ej1.py
@@ -0,0 +1,2 @@
+print("\U0001f40d") #codigo para poner emojis y cpsas asi
+print("holiiiiiiiiiii".rstrip("i")) #si una letra p letras estran a lo ultimo las sacas y asi qsy
---
+++ Ej2.py
@@ -0,0 +1,3 @@
+personas = ["Mati", "Rami", "Amarusin"]
+for e in personas: 
+    print(f"Hola {e} veni a cenar jaja")
