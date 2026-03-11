c1ffa1ca84d219af87bc7ad8897ab4e2b7d14d3c
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 10:20:28 -0300

    si ok

---
+++ Ej16.py
@@ -0,0 +1,9 @@
+rios= {
+    "Nilo" : "Egipto",
+    "Rio Uruguay" : "Uruguay",
+    "Rio Paraguay" : "Paraguay"
+}
+for e in rios:
+    print(f"El río {e} pasa por {rios[e]}")
+    print(e)
+    print(rios[e])
