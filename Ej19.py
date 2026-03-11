d53836b4b94127f759363783bd1d6a6cefc61b0f
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 10:53:36 -0300

    no lo termine todaviua este eh

---
+++ Ej19.py
@@ -0,0 +1,23 @@
+CABAxs  = {
+    "Pais" : "Argentina",
+    "Poblacion" : "3 millones",
+    "Dato" : "Tiene una de las avenidas mas anchas del mundo, 'Av. 9 de Julio'."
+}
+
+paris = {
+    "Pais" : "Francia",
+    "Poblacion" : "2 millones",
+    "Dato" : "Tiene las Catacumbas de Paris abajo."
+}
+
+nuevayk = {
+    "Pais" : "EEUU",
+    "Poblacion" : "8 millones",
+    "Dato" : "Hablan mas de 800 idiomas"
+}
+
+ciuds = {
+    "CABA" : CABAxs,
+    "Paris" : paris,
+    "Nueva York" : nuevayk
+}
