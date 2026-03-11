5d6e6f5065892b9ebc5a8ec75d3b176a687623e6
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 10:45:03 -0300

    mascotas

---
+++ Ej18.py
@@ -0,0 +1,22 @@
+Shushu = {
+    "Nombre" : "Shushu",
+    "Raza" : "Perro",
+    "Dueño" : "Rami"
+}
+
+Zyra = {
+    "Nombre" : "Zyra",
+    "Raza" : "Gato",
+    "Dueño" : "Amarusin"
+}
+
+Mario = {
+    "Nombre" : "Mario",
+    "Raza" : "Gato",
+    "Dueño" : "Mati"
+}
+
+gente = [Shushu, Zyra, Mario]
+
+for e in gente:
+    print(e)
