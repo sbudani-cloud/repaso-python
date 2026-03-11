0b96f1efb026881223d23ba238a0857712652863
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:49:18 -0300

    nenecitamos mas dusuarisos

--- Ej10.py
+++ Ej10.py
@@ -1,6 +1,9 @@
 usuarios = ["la carbita", "EL ADMIN", "yo", "alguien mas"]
-for e in usuarios:
-    if e == "EL ADMIN":
-        print("Hola admin queres configurar cosas re locas jaja sos adminm")
-    else:
-        print("Bienvenido", e)
+if usuarios == []:
+    print("necesitmaos mas usuarioossos!ª!!!!!!!!!!! QUE MANDANNNNN")
+else:
+    for e in usuarios:
+        if e == "EL ADMIN":
+            print("Hola admin queres configurar cosas re locas jaja sos adminm")
+        else:
+            print("Bienvenido", e)
