1be0fd33e6cc8e006df08606c806e5734060af4a
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:23:43 -0300

    amiguizzas

---
+++ Ej4b.py
@@ -0,0 +1,8 @@
+pizzas = ["Pizza de Jamón y Morrón", "Fugazzetta", "Pizza de Muzzarella", "Pizza Napolitana"]
+amiguizzas = ["Pizza de Jamón y Morrón", "Fugazzetta", "Pizza de Muzzarella", "Pizza sin salsa, queso ni masa"]
+print("Mis pizzas favoritas son:")
+for e in pizzas:
+    print(e)
+print("Mis pizzas favoritas de mi amigo son:")
+for e in amiguizzas:
+    print(e)
