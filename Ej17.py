4b08b4ccd4eb5f96de9c2ace92543602274cef03
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 10:36:15 -0300

    javier miguel

---
+++ Ej17.py
@@ -0,0 +1,14 @@
+leng_favs = { #ninguno de estos datos esta verificado con las personas reales. cabe recalcar
+    "Amarusin" : "El lenguaje del amor y la paz",
+    "Mati" : "html",
+    "Rami" : "c",
+    "Un bebe" : "Python" #volvio un bebe de vuelta
+}
+#SE QUE ENCUENSTADOS **NOOO** ES UNA PALABRA REAL, ES UN CHISTE
+encuenstaditos = ["Amarusin", "lol", "javier miguel"] #SE QUE ES JAVIEL MILEI ES UN CHISTE TAMBIEN
+
+for e in encuenstaditos:
+    if e in leng_favs:
+        print(f"{e} gracias pro respodner")
+    else:
+        print(f"che {e} plz hace la encuesta")
