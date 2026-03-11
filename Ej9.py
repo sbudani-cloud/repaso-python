18626f2f0986ba7b0a16825a35de77e2ba785fd9
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:43:17 -0300

    a partir de el siguiuetne commit vuelv a mi PROFESOIONALDIAD

---
+++ Ej9.py
@@ -0,0 +1,13 @@
+frutas = ["Frutilla", "Sandia", "Pera"]
+if "Frutilla" in frutas:
+    print("te gustan las frutillitas jad")
+else:
+    print("no te fgustan las frutillas eh que te pasa que te pasa cn las manzanas")
+if "Pera" in frutas:
+    print("entonce te gustan las peras ok esta bien")
+else:
+    print("no te gustan? ok no te gustan las peras ok")
+if "Sandia" in frutas:
+    print("si lo pensas sandia es como si un señor se llamara dia y fuera el San Dia. bueno eso")
+else:
+    print("euu que te pa con el San Dia")
