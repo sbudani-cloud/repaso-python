643ed5cb42c1c79c8f19e7d37a7db9fc45ede3fe
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:59:40 -0300

    CREO que asi tenia q hacerse el ejercicio

---
+++ Ej12.py
@@ -0,0 +1,20 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
+for i in nums:
+    if i == 1:
+        print("1ro")
+    elif i == 2:
+        print("2do")
+    elif i == 3:
+        print("3ro")
+    elif i == 4:
+        print("4to")
+    elif i == 5:
+        print("5to")
+    elif i == 6:
+        print("6to")
+    elif i == 7:
+        print("7mo")
+    elif i == 8:
+        print("8vo")
+    elif i == 9:
+        print("9no")
