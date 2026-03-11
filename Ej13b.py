bb16622ce8426b425a207c6b107c2e7c6a48f64f
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 10:39:57 -0300

    lalsdlasdla

---
+++ Ej13b.py
@@ -0,0 +1,25 @@
+Amarusin = {
+    "Nombre" : "Amaru",
+    "Apellido" : "Budani",
+    "Edad" : 14,
+    "Ciudad" : "CABA"
+}
+
+Rami = {
+    "Nombre" : "Ramiro",
+    "Apellido" : "Budani",
+    "Edad" : 20,
+    "Ciudad" : "CABA"
+}
+
+Mati = {
+    "Nombre" : "Matias",
+    "Apellido" : "Budani",
+    "Edad" : 18,
+    "Ciudad" : "CABA"
+}
+
+gente = [Amarusin, Rami, Mati]
+
+for e in gente:
+    print(e)
