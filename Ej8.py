35963249de656644818d781c108a43c471cff9b8
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:37:41 -0300

    profe no vas a ver esto no?

---
+++ Ej8.py
@@ -0,0 +1,15 @@
+import random
+edad = random.randint(1, 99)
+print(edad)
+if edad < 2:
+    print("Y EL BEBEBEE??? SOS UN BEBEEEEEE")
+elif edad >= 2 and edad < 4:
+    print("sos un nenito chiquitooo hashahajjaja jaaaaa")
+elif edad >= 4 and edad < 13:
+    print("sos un nene como el chabon que se llama nene malo excepto que el no es u  nene pero vis si sos un nene o una nena o genero neutro o alguna otra opcion no se")
+elif edad >= 13 and edad < 20:
+    print("sos un adolencete hasjdsajdiasd no se que decir aca")
+elif edad >= 20 and edad < 65:
+    print("adultito lalal laburo tramites lkalala blabla")
+else:
+    print("ya estas mayorcito ehh..")
