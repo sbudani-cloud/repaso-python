8a49571336f0f5b60f1ae7dc7ffb8582b65bf162
“sbudani-cloud”~ <sbudani@alumno.huergo.edu.ar>
2026-03-11 09:46:11 -0300

    ok ya al siguiente prometo preofeisonalidad deah a quien le hablo hola hola hola sjdajs

---
+++ Ej10.py
@@ -0,0 +1,6 @@
+usuarios = ["la carbita", "EL ADMIN", "yo", "alguien mas"]
+for e in usuarios:
+    if e == "EL ADMIN":
+        print("Hola admin queres configurar cosas re locas jaja sos adminm")
+    else:
+        print("Bienvenido", e)
