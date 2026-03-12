c5c131afa537f01e0c73cc5878697a1655530648
sbudani-cloud <sbudani@alumno.huergo.edu.ar>
2026-03-12 15:34:01 -0300

    a

--- Ej33.py
+++ Ej33.py
@@ -1,3 +1,4 @@
+#Ejercicios para hacer (nº de diapositiva): 223, 232, 262, 288, 312, 315, 348, 349\
 class Restaurante:
     def __init__(self, nombre, tipo_cocina):
         self.nombre = nombre
@@ -12,9 +13,16 @@ class Restaurante:
     def incrementar_clientes_atendidos(self, num):
         self.clientes_atendidos += num
 
+class PuestoDeHelados(Restaurante):
+    def __init__(self, nombre, tipo_cocina, sabor):
+        super().__init__(nombre, tipo_cocina)
+        self.sabores = sabor
+    def mostrar_sabores(self):
+        print("Sabores:")
+        for e in self.sabores:
+            print(e)
+
 amarusin = Restaurante("Los Amarusines Bebes", "Italiana")
-amarusin.describir_restaurante()
-amarusin.abrir_restaurante()
-amarusin.establecer_clientes_atendidos(4)
-amarusin.incrementar_clientes_atendidos(2)
-amarusin.describir_restaurante()
+helados_amarusin = PuestoDeHelados("Los Amarusines Bebes", "Heladería", ["Frutilla", "Vainilla", "Chocolate"])
+
+helados_amarusin.mostrar_sabores()
