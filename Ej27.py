bfeed3ec723ebe942a23d09ce8bf219d0228fc7e
sbudani-cloud <sbudani@alumno.huergo.edu.ar>
2026-03-12 14:44:05 -0300

    151

--- Ej27.py
+++ Ej27.py
@@ -1,2 +1,12 @@
 #Ejercicios para hacer (nº de diapositiva): 151, 152, 173, 175, 193, 195, 205, 223, 232, 262, 288, 
 #312, 315, 348, 349
+def hacer_album(artista, titulo):
+    album = {
+        "Artista" : artista,
+        "Título" : titulo
+    }
+    print(f"Título: {album["Título"]}\nArtista: {album["Artista"]}\n")
+
+hacer_album("Mitski", "Be the Cowboy")
+hacer_album("Mitski", "Lush")
+hacer_album("Mitski", "Puberty 2")
