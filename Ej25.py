94fe1f415ab773a98cec9d22d91f9f197d54124d
sbudani-cloud <sbudani@alumno.huergo.edu.ar>
2026-03-12 13:52:49 -0300

    lol

--- Ej25.py
+++ Ej25.py
@@ -1,8 +1,13 @@
-sanguches_pedidos = ["Jamón y queso", "Atún", "Milanesa", "BLT", "Lomito", "Vegetariano"]
+
+sanguches_pedidos = ["Jamón y queso", "Pastrón", "Atún", "Milanesa", "Pastrón", "BLT", "Lomito", "Pastrón", "Vegetariano"]
 sanguches_terminados = []
+print("Nos quedamos sin Pastrón")
 while sanguches_pedidos:
     sanguchito = sanguches_pedidos.pop()
-    print(f"Tu sanguche de {sanguchito} está listo!")
-    sanguches_terminados.append(sanguchito)
-    input()
+    if sanguchito == "Pastrón":
+        pass
+    else:
+        print(f"Tu sanguche de {sanguchito} está listo!")
+        sanguches_terminados.append(sanguchito)
+        input()
 print(sanguches_terminados)
